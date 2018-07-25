1d81f617c51cae70e5fbdae7f29f5351a59d3a30
Yousef <yousef@Apples-MacBook-Pro-2.local>
2018-07-26 00:24:46 +0300

    mnawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { LoginPage } from '../pages/login/login';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
 import { ResidenceProfilePage } from '../pages/residence-profile/residence-profile';
 @Component({
@@ -12,13 +12,32 @@ import { ResidenceProfilePage } from '../pages/residence-profile/residence-profi
 export class MyApp {
   rootPage:any = ResidenceProfilePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, afAuth: AngularFireAuth, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      afAuth.authState.subscribe(
+        user => {
+          if (user) {
+            this.rootPage = HomePage;
+          } else {
+            this.rootPage = ResidenceProfilePage;
+          }
+          statusBar.styleDefault();
+          splashScreen.hide();
+        },
+        () => {
+          this.rootPage = LoginPage;
+          statusBar.styleDefault();
+          splashScreen.hide();
+        }
+      )
     });
+    
   }
+
+  
+  
+ 
 }
 
